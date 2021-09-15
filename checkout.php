13b3e90710d29bda295e75519a2b8ceae21955f5
bintangx1902 <60544749+bintangx1902@users.noreply.github.com>
2021-09-15 22:26:32 +0700

    Update

--- checkout.php
+++ checkout.php
@@ -21,8 +21,73 @@
 
     <div class="container my-5">
         <div class="card border-0">
-            <div class="card-body">
-                <div class="jumbotron"></div>
+            <div class="card-body ">
+                <h4 class="card-title mt-1">Checkout & pembayaran</h4>
+                <form action="">
+                    <div class="form-group row">
+                        <label for="inputEmail3" class="col-sm-2 col-form-label">Dengan Dokter : </label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="inputEmail3" value="dr. Yahya Lengkong SpKJ." disabled>
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label" for="ddate">Pada Tanggal : </label>
+                        <div class="col-sm-10">
+                            <input type="date" name="" value="2021-09-27" class="form-control">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="ttime" class="col-sm-2 col-form-label">Pada Jam : </label>
+                        <div class="col-sm-2">
+                            <input type="time" class="form-control">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="prc" class="col-sm-2 col-form-label">Biaya Konsultasi : </label>
+                        <div class="col-sm-10">
+                            <input type="text" value="Rp. 350.000,-" class="form-control" disabled>
+                        </div>
+                    </div>
+
+                    <fieldset class="form-group row">
+                        <legend class="col-form-label col-sm-2 float-sm-left pt-0">Metode Pembayaran</legend>
+                        <div class="col-sm-10">
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+                                <label class="form-check-label" for="gridRadios1">
+                                    Pembayaran Online Via bank
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
+                                <label class="form-check-label" for="gridRadios2">
+                                    Bayar di Tempat
+                                </label>
+                            </div>
+                            <div class="form-check disabled">
+                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled>
+                                <label class="form-check-label" for="gridRadios3">
+                                    Pembayaran dengan <em>E-Wallet</em>
+                                </label>
+                            </div>
+                        </div>
+                    </fieldset>
+
+                    <div class="form-group row">
+                        <label for="v-acc" class="col-sm-2 col-form-label">Nomor Pembayaran </label>
+                        <div class="col-sm-10">
+                            <input type="text" value="23344455555" class="form-control" disabled>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <button class="btn btn-primary">Lakukan Pembayaran</button>
+                    </div>
+
+                </form>
 
             </div>
         </div>
