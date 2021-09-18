7696a919904f8a323c2a059c25af4627c99fd9c5
bintangx1902 <60544749+bintangx1902@users.noreply.github.com>
2021-09-18 18:19:16 +0700

    Webcam Update

--- jadwal-saya.php
+++ jadwal-saya.php
@@ -24,55 +24,57 @@
 
         <div class="card border-0">
             <div class="card-body">
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Dengan Dokter</th>
-                            <th scope="col">Pada Jam dan Tanggal</th>
-                            <th scope="col">Metode </th>
-                            <th scope="col">Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php for ($sc = 0; $sc < 6; $sc++) { ?>
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                        <thead>
                             <tr>
-                                <th scope="row"><?php echo $sc; ?></th>
-                                <td>dr. Yahya Lengkong SpKj.</td>
-                                <td>
-                                    <?php
-                                    if ($sc >= 3) { ?>
-                                        30 September 2021 - 15:30
-                                    <?php
-                                    } else { ?>
-                                        25 September 2021 - 10.45
-                                    <?php } ?>
-                                </td>
-                                <td>
-                                    <?php if ($sc < 3) {
-                                        if ($sc == 0) { ?>
-                                            <a href="meet.php"><span class="text-primary"><i class="icon ion-videocamera"></i> <em>Online Meet</em></span></a>
-                                        <?php } else { ?>
-                                            <span class="text-secondary"><i class="icon ion-videocamera"></i> <em>Online Meet</em></span>
-                                        <?php } ?>
-
-                                    <?php } else { ?>
-                                        <span class="text-info"><i class="icon ion-ios-stopwatch"></i> Temu Langsung</span>
-                                    <?php } ?>
-                                </td>
-                                <td>
-                                    <?php
-                                    if ($sc >= 2) { ?>
-                                        <span class="text-success"><i class="icon ion-checkmark-round"></i> Sudah Selesai</span>
-                                    <?php
-                                    } else { ?>
-                                        <span class="text-warning"><i class="icon ion-load-d"> Sesuai Jadwal</i></span>
-                                    <?php } ?>
-                                </td>
+                                <th scope="col">#</th>
+                                <th scope="col">Dengan Dokter</th>
+                                <th scope="col">Pada Jam dan Tanggal</th>
+                                <th scope="col">Metode </th>
+                                <th scope="col">Status</th>
                             </tr>
-                        <?php } ?>
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            <?php for ($sc = 0; $sc < 6; $sc++) { ?>
+                                <tr>
+                                    <th scope="row"><?php echo $sc; ?></th>
+                                    <td>dr. Yahya Lengkong SpKj.</td>
+                                    <td>
+                                        <?php
+                                        if ($sc >= 3) { ?>
+                                            30 September 2021 - 15:30
+                                        <?php
+                                        } else { ?>
+                                            25 September 2021 - 10.45
+                                        <?php } ?>
+                                    </td>
+                                    <td>
+                                        <?php if ($sc < 3) {
+                                            if ($sc == 0) { ?>
+                                                <a href="meet.php"><span class="text-primary"><i class="icon ion-videocamera"></i> <em>Online Meet</em></span></a>
+                                            <?php } else { ?>
+                                                <span class="text-secondary"><i class="icon ion-videocamera"></i> <em>Online Meet</em></span>
+                                            <?php } ?>
+
+                                        <?php } else { ?>
+                                            <span class="text-info"><i class="icon ion-ios-stopwatch"></i> Temu Langsung</span>
+                                        <?php } ?>
+                                    </td>
+                                    <td>
+                                        <?php
+                                        if ($sc >= 2) { ?>
+                                            <span class="text-success"><i class="icon ion-checkmark-round"></i> Sudah Selesai</span>
+                                        <?php
+                                        } else { ?>
+                                            <span class="text-warning"><i class="icon ion-load-d"> Sesuai Jadwal</i></span>
+                                        <?php } ?>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
 
--- meet.php
+++ meet.php
@@ -3,11 +3,23 @@
 <head>
     <?php include "meta.php"; ?>
     <title>Meet Dengan Dokter</title>
+    <link rel="stylesheet" href="assets/css/floating.css">
 </head>
 
 <body>
     <?php include "head.php"; ?>
+
+
+    <div class="mybg">
+        <div class="container">
+            <img src="assets/img/dctr-example.jpg" alt="dokter" class="img-fluid">
+        </div>
+
+    </div>
+
     <?php include "footer.php"; ?>
+    <div class="d-flex justify-content-center float"><video autoplay="true" id="videoElement" class="img-fluid"></video></div>
+    <script src="assets/js/webcam.js"></script>
 </body>
 
 </html>
